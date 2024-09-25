3c54a0657b8cc631fd5675fd3159cc8d581e37d9
prtiyushprakash <127317560+prtiyushprakash@users.noreply.github.com>
2024-09-26 01:04:16 +0530

    Update bgmi.py

--- bgmi.py
+++ bgmi.py
@@ -13,10 +13,10 @@ import string
 logging.basicConfig(filename='bot.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('6400615750:AAHero6NP7UImczIeqmVL76yh4zw2tP893s')
+bot = telebot.TeleBot('7473476996:AAFgSnsy4C8kne5wbjRPWYPx5Ra66dDu7f4')
 # Owner and admin user IDs
-owner_id = "6063791789"
-admin_ids = ["6516178450" "708030615"]
+owner_id = "7416009186"
+admin_ids = ["7416009186"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
